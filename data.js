5e6ae0f631a9c61e70b1659aa7e06e61f2db9331
danilovicz <42356725+danilovicz@users.noreply.github.com>
2018-09-30 18:09:22 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,314 @@
+var log = console.log();
+
+
+//Array of objects
+var friendslist = [
+  {
+    "id": 1,
+    "firstName": "Paul",
+    "surname": "Crowe",
+    "age": 28,
+    "gender": "male",
+    "friends": [
+      2
+    ]
+  },
+  {
+    "id": 2,
+    "firstName": "Rob",
+    "surname": "Fitz",
+    "age": 23,
+    "gender": "male",
+    "friends": [
+      1,
+      3
+    ]
+  },
+  {
+    "id": 3,
+    "firstName": "Ben",
+    "surname": "O'Carolan",
+    "age": null,
+    "gender": "male",
+    "friends": [
+      2,
+      4,
+      5,
+      7
+    ]
+  },
+  {
+    "id": 4,
+    "firstName": "Victor",
+    "surname": "",
+    "age": 28,
+    "gender": "male",
+    "friends": [
+      3
+    ]
+  },
+  {
+    "id": 5,
+    "firstName": "Peter",
+    "surname": "Mac",
+    "age": 29,
+    "gender": "male",
+    "friends": [
+      3,
+      6,
+      11,
+      10,
+      7
+    ]
+  },
+  {
+    "id": 6,
+    "firstName": "John",
+    "surname": "Barry",
+    "age": 18,
+    "gender": "male",
+    "friends": [
+      5
+    ]
+  },
+  {
+    "id": 7,
+    "firstName": "Sarah",
+    "surname": "Lane",
+    "age": 30,
+    "gender": "female",
+    "friends": [
+      3,
+      5,
+      20,
+      12,
+      8
+    ]
+  },
+  {
+    "id": 8,
+    "firstName": "Susan",
+    "surname": "Downe",
+    "age": 28,
+    "gender": "female",
+    "friends": [
+      7
+    ]
+  },
+  {
+    "id": 9,
+    "firstName": "Jack",
+    "surname": "Stam",
+    "age": 28,
+    "gender": "male",
+    "friends": [
+      12
+    ]
+  },
+  {
+    "id": 10,
+    "firstName": "Amy",
+    "surname": "Lane",
+    "age": 24,
+    "gender": "female",
+    "friends": [
+      5,
+      11
+    ]
+  },
+  {
+    "id": 11,
+    "firstName": "Sandra",
+    "surname": "Phelan",
+    "age": 28,
+    "gender": "female",
+    "friends": [
+      5,
+      10,
+      19,
+      20
+    ]
+  },
+  {
+    "id": 12,
+    "firstName": "Laura",
+    "surname": "Murphy",
+    "age": 33,
+    "gender": "female",
+    "friends": [
+      7,
+      9,
+      13,
+      20
+    ]
+  },
+  {
+    "id": 13,
+    "firstName": "Lisa",
+    "surname": "Daly",
+    "age": 28,
+    "gender": "female",
+    "friends": [
+      12,
+      14,
+      20
+    ]
+  },
+  {
+    "id": 14,
+    "firstName": "Mark",
+    "surname": "Johnson",
+    "age": 28,
+    "gender": "male",
+    "friends": [
+      13,
+      15
+    ]
+  },
+  {
+    "id": 15,
+    "firstName": "Seamus",
+    "surname": "Crowe",
+    "age": 24,
+    "gender": "male",
+    "friends": [
+      14
+    ]
+  },
+  {
+    "id": 16,
+    "firstName": "Daren",
+    "surname": "Slater",
+    "age": 28,
+    "gender": "male",
+    "friends": [
+      18,
+      20
+    ]
+  },
+  {
+    "id": 17,
+    "firstName": "Dara",
+    "surname": "Zoltan",
+    "age": 48,
+    "gender": "male",
+    "friends": [
+      18,
+      20
+    ]
+  },
+  {
+    "id": 18,
+    "firstName": "Marie",
+    "surname": "D",
+    "age": 28,
+    "gender": "female",
+    "friends": [
+      17
+    ]
+  },
+  {
+    "id": 19,
+    "firstName": "Catriona",
+    "surname": "Long",
+    "age": 28,
+    "gender": "female",
+    "friends": [
+      11,
+      20
+    ]
+  },
+  {
+    "id": 20,
+    "firstName": "Katy",
+    "surname": "Couch",
+    "age": 28,
+    "gender": "female",
+    "friends": [
+      7,
+      11,
+      12,
+      13,
+      16,
+      17,
+      19
+    ]
+  }
+
+];
+
+
+
+var id= 20; //window.prompt("Choose User Id");;
+
+
+//creating f-on, with id and userList as parameters
+function getFriends(id,userList){
+  var friends=[];
+  var friendsIds=[];//for keeping data
+
+  for (var i = 0; i < userList.length; i++) { //looping through userList
+
+    if (userList[i].id==id) //searching specific id inside userList
+        friendsIds = userList[i].friends; //saving data inside friendsIds
+
+    }
+
+
+
+  for (var i = 0; i < friendsIds.length; i++) { //looping through friendsIds
+      //using array.prototype.find method to find element
+      var found = userList.find(function(element) {
+        return element.id ===friendsIds[i];
+    });
+      friends.push(found); // using push method to store data in friends
+//za html friends[1].id...
+    }
+
+  return friends;
+
+}
+
+//console.log(friends);
+
+//friends of friends
+var commonFriends=[];
+var friends = getFriends(20, friendslist);
+//console.log(friends); // friends of user 20
+
+for (var i = 0; i < friends.length; i++) {
+  var newFriends = getFriends(friends[i].id,friendslist);
+
+  //using for cycle to store data as an obj. not as array
+  for (var j = 0; j < newFriends.length; j++) {
+
+    var found = commonFriends.find(function(element) {
+      return element.id ===newFriends[j].id;
+    });
+    if (!found) {
+        commonFriends.push(newFriends[j]);
+      }
+  }
+}
+//console.log(commonFriends);
+
+
+//suggested friends
+var peopleYouMayKnow = [];
+var recommended = getFriends(20, friendslist);
+
+for (var i = 0; i < commonFriends.length; i++) {
+  var recoFriends = getFriends(commonFriends[i].id,friendslist);
+
+  for (var j = 0; j < recoFriends.length; j++) {
+
+    var found = peopleYouMayKnow.find(function(element) {
+      return element.id ===recoFriends[j].id;
+    });
+    if (!found) {
+        peopleYouMayKnow.push(recoFriends[j]);
+      }
+  }
+
+}
+console.log(peopleYouMayKnow);
